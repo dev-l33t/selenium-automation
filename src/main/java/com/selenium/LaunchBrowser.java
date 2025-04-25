b21813394977a9270f108698c0b17f58a9259049
dev-l33t <desk.pratikjoshi@gmail.com>
2025-04-25 21:37:18 +0530

    Create LaunchBrowser.java

---
+++ src/main/java/com/selenium/LaunchBrowser.java
@@ -0,0 +1,24 @@
+package com.selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class LaunchBrowser {
+    public static void main(String[] args) {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless", "--no-sandbox", "--disable-dev-shm-usage");
+        WebDriver driver = new ChromeDriver(options);
+
+        driver.get("https://www.google.com");
+        WebElement searchBox = driver.findElement(By.name("q"));
+        searchBox.sendKeys("Selenium GitHub Actions demo");
+        searchBox.submit();
+
+        System.out.println("Page title is: " + driver.getTitle());
+
+        driver.quit();
+    }
+}
