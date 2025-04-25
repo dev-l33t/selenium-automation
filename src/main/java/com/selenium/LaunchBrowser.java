2d388e4864b9d3a6e437de5fcff2b27c4356a6a2
dev-l33t <desk.pratikjoshi@gmail.com>
2025-04-26 01:23:09 +0530

    Update LaunchBrowser.java

--- src/main/java/com/selenium/LaunchBrowser.java
+++ src/main/java/com/selenium/LaunchBrowser.java
@@ -7,26 +7,19 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 public class LaunchBrowser {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException{
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--headless", "--no-sandbox", "--disable-dev-shm-usage");
         WebDriver driver = new ChromeDriver(options);
 
         driver.get("https://www.google.com");
         WebElement searchBox = driver.findElement(By.name("q"));
-        try {
-              Thread.sleep(2000);
-            } catch (InterruptedException e) {
-                    e.printStackTrace();
-                    }
-
+      
+        Thread.sleep(2000); // wait for 2 seconds
         searchBox.sendKeys("pratiks-desk.site");
         searchBox.submit();
-
+        Thread.sleep(3000); // wait for 2 seconds
         System.out.println("Page title is: " + driver.getTitle());
-        System.out.println("=== PAGE TITLE: " + driver.getTitle() + " ===");
-
-
         driver.quit();
     }
 }
