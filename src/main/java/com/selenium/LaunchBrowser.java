beab515a65c8a2dd6cbc5eefaa385dcb8bf5e9f5
dev-l33t <desk.pratikjoshi@gmail.com>
2025-04-26 01:30:51 +0530

    Update LaunchBrowser.java

--- src/main/java/com/selenium/LaunchBrowser.java
+++ src/main/java/com/selenium/LaunchBrowser.java
@@ -9,7 +9,12 @@ import org.openqa.selenium.chrome.ChromeOptions;
 public class LaunchBrowser {
     public static void main(String[] args) throws InterruptedException{
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless", "--no-sandbox", "--disable-dev-shm-usage");
+        options.addArguments("--headless=new"); // or "--headless=chrome" depending on your version
+        options.addArguments("--disable-gpu");
+        options.addArguments("--window-size=1920,1080");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+
         WebDriver driver = new ChromeDriver(options);
 
         driver.get("https://www.google.com");
@@ -20,6 +25,9 @@ public class LaunchBrowser {
         searchBox.submit();
         Thread.sleep(10000); // wait for 10 seconds
         System.out.println("Page title is: " + driver.getTitle());
+        Thread.sleep(2000);
+        System.out.println("After wait title: " + driver.getTitle());
+
         driver.quit();
     }
 }
